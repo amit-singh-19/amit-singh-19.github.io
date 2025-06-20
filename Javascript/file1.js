bb30ef052e35e8530a8064eec1061c5b8fe6cbc5
amit-singh-19 <vinitsingh4477@gmail.com>
2025-06-20 09:58:31 +0530

    Updated

--- Javascript/file1.js
+++ Javascript/file1.js
@@ -1,1 +1,24 @@
-console.log('Hello ');
+let studentName = "John";
+let age = 20;
+let prometed = true;
+let score;
+let city = null;
+
+// console.log(studentName);
+// console.log(typeof(studentName));
+// console.log(typeof score); //undefined
+// console.log(typeof city);  //object
+
+// console.log(typeof age);
+// let strAge = String(age);    //type conversion
+// console.log(typeof strAge);
+
+// let rating = '5'
+// console.log(typeof rating);
+// let strRating = Number(rating)
+// console.log(typeof strRating);
+
+// let flag = "true";
+// let boolFlag = Boolean(flag);
+// console.log(typeof boolFlag);
+
---
+++ Javascript/file2.js
@@ -0,0 +1,39 @@
+// let a = 50;
+// let b = 10;
+// console.log(a+b);
+
+// Assignment Operator
+
+// let a = "2";
+// let b = 2
+// console.log(a == b);  //true
+
+// let a = "2";
+// let b = 2
+// console.log(a === b);  //false
+
+// let a = 10;
+// let b = 2;
+// console.log(a !== b);
+
+// Logical Operator
+
+// let a = 10;
+// let b = 2;
+// let c = 12;
+// console.log(a > b && a > c);
+
+// let a = 10;
+// let b = 2;
+// let c = 12;
+// console.log(a > b || a > c);
+
+// let a = 10;
+// let b = 2;
+// let c = 12;
+// console.log(!a>b);
+
+let a = 10;
+let b = 2;
+let c = 12;
+console.log(a**b);
