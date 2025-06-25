9f010a21b2af5ed383dcd6ac49686c16ac2d6650
amit-singh-19 <vinitsingh4477@gmail.com>
2025-06-25 23:20:36 +0530

    Enhance shopping cart functionality with navigation and display updates; add MongoDB commands to Notes.txt

--- Javascript/shoppingCart/app.js
+++ Javascript/shoppingCart/app.js
@@ -9,15 +9,40 @@ const products = [
 const productsContainer = document.getElementById("product-container");
 const cartContainer = document.getElementById("cart-container");
 const orderTotal = document.getElementById("order-total");
+const homeSection = document.getElementById("home-section");
+const cartSection = document.getElementById("cart-section");
+
+const showHome = () => {
+  homeSection.style.display = "block";
+  cartSection.style.display = "none";
+};
+
+const showCartProducts = () => {
+  homeSection.style.display = "none";
+  cartSection.style.display = "block";
+
+  showCart();
+};
 
 const showProducts = () => {
   productsContainer.innerHTML = "";
   products.map((value) => {
     const div = document.createElement("div");
     div.className = "product-item";
-    div.innerHTML = `
-      <span> ${value.name} - ₹${value.price}</span>
-      <button onclick = "addToCart(${value.id})" class="addtocart-btn">Add To Cart</button>`;
+    if (cart[value.id]) {
+      div.innerHTML = `
+        <span> ${value.name} - ₹${value.price}</span>
+        <div class="add-cart-btn">
+        <button onclick= "decrement(${value.id})" class="cart-btn">-</button> 
+        <span class="cart-span">${cart[value.id]}</span> 
+        <button onclick= "increment(${value.id})" class="cart-btn">+</button> 
+        </div>
+      `;
+    } else {
+      div.innerHTML = `
+        <span> ${value.name} - ₹${value.price}</span>
+        <button onclick = "addToCart(${value.id})" class="addtocart-btn">Add To Cart</button>`;
+    }
     productsContainer.appendChild(div);
   });
 };
@@ -25,6 +50,7 @@ showProducts();
 
 const addToCart = (id) => {
   cart = { ...cart, [id]: 1 };
+  showProducts();
   showCart();
 };
 
@@ -38,7 +64,7 @@ const showCart = () => {
         <span> ${value.name} - ₹${value.price} x ${cart[value.id]}  =  ₹${
         cart[value.id] * value.price
       } </span>
-        <div>
+        <div class="add-cart-btn">
         <button onclick= "decrement(${value.id})" class="cart-btn">-</button> 
         <span class="cart-span">${cart[value.id]}</span> 
         <button onclick= "increment(${value.id})" class="cart-btn">+</button> 
@@ -53,11 +79,13 @@ const showCart = () => {
 const increment = (id) => {
   cart = { ...cart, [id]: cart[id] + 1 };
   showCart();
+  showProducts();
 };
 
 const decrement = (id) => {
   cart = { ...cart, [id]: cart[id] - 1 };
   showCart();
+  showProducts();
 };
 
 const orderValue = () => {
