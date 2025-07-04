07e0d814efe6b7a13240dfcca153c17d56eb6d5d
amit-singh-19 <vinitsingh4477@gmail.com>
2025-07-05 00:56:36 +0530

    Refactor user registration and login functionality; improve error handling and password hashing

--- NodeJS/nodeapp/index11.js
+++ NodeJS/nodeapp/index11.js
@@ -3,50 +3,20 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 const app = express();
-const SECRET_KEY = "mysecrectkey";
+const SECRET_KEY = "mysecretkey";
 
 const users = [];
 
 app.use(express.json());
 
-//Register User-
-app.post("/register", async (req, res) => {
-  const { name, email, pass, role } = req.body;
-  const hashedPass = await bcrypt.hash(pass, SECRET_KEY);
-  try {
-    users.push({
-      name,
-      email,
-      pass: hashedPass,
-      role,
-    });
-    res.status(201).json({ message: "Register Successfull" });
-  } catch (error) {
-    res.status(400).json({message: "Something went wrong"})
-  }
-});
-
-// Login Route-
-app.post("/login", (req, res) => {
-  const { email, pass } = req.body;
-  const found = users.find(
-    (user) => user.email === email && user.pass === pass
-  );
-  if (found) {
-    const token = jwt.sign(found, SECRET_KEY, { expiresIn: "1h" });
-    res.status(200).json({ message: "Login Successfull", token });
-  } else {
-    res.status(403).json({ message: "Invalid Credentials" });
-  }
-});
-
 //Middleware-
 const authenticate = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
+    if (!authHeader) res.status(401).json({ message: "No token provided" });
     const token = authHeader.split(" ")[1];
     const user = jwt.verify(token, SECRET_KEY);
-    req.user = user.role;
+    req.user = user;
     next();
   } catch (error) {
     return res.status(400).json({ message: "Invaid Token" });
@@ -55,13 +25,46 @@ const authenticate = (req, res, next) => {
 
 const authorize = (role) => {
   return (req, res, next) => {
-    if (req.role === "admin") {
+    if (req.user.role === role) {
       next();
     } else {
       res.status(403).json({ message: "Unauthorized Access" });
     }
   };
 };
+
+//Register User-
+app.post("/register", async (req, res) => {
+  const { name, email, pass, role } = req.body;
+  try {
+    const hashedPass = await bcrypt.hash(pass, 10);
+    users.push({
+      name,
+      email,
+      pass: hashedPass,
+      role,
+    });
+    res.status(201).json({ message: "Register Successful" });
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+// Login User-
+app.post("/login", async (req, res) => {
+  const { email, pass } = req.body;
+  const user = users.find((user) => user.email === email);
+
+  if (!user) res.status(401).json({ message: "User not found" });
+
+  const isMatch = await bcrypt.compare(pass, user.pass);
+  if (isMatch) {
+    const token = jwt.sign(user, SECRET_KEY, { expiresIn: "1h" });
+    res.status(200).json({ message: "Login Successfull", token });
+  } else {
+    res.status(403).json({ message: "Invalid Credentials" });
+  }
+});
 
 // Protected Dashboard
 app.get("/dashboard", authenticate, authorize("admin"), (req, res) => {
--- NodeJS/nodeapp/index9.js
+++ NodeJS/nodeapp/index9.js
@@ -1,6 +1,7 @@
-//bcrypt module- use to hash password 
+//bcrypt module- use to hash password
 import bcrypt from "bcrypt";
 
+/*
 const pwd = "pass1234";
 
 //bcrypt return promise that's why we have used await here
@@ -9,3 +10,47 @@ const pwd = "pass1234";
 
 const check = await bcrypt.compare(pwd, "$2b$10$ZyIj1RB3cAfTOjf3smpQOue.ttsXs42wJ4Cj7o9XfbtjIv5IJXZ5C");
 console.log(check)
+
+*/
+
+import express from "express";
+const app = express();
+app.use(express.json());
+
+//Sign up
+const users = [];
+app.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+    users.push({ name, email, hashPassword });
+    res.status(201).json({ message: "User Register Successful", users });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong!" });
+  }
+});
+
+//login
+app.post("/login", async (req, res) => {
+  const users = {
+    name: "Amit",
+    email: "amit@gmail.com",
+    hashPassword:
+      "$2b$10$lVDWFJkYzXjBo09lR48KCOgdG0d8imCPB03b50tA26mKwjuTheiZG"
+  };
+  try {
+    const { email, password } = req.body;
+    if (email !== users.email)
+      res.status(400).json({ message: "Email not found" });
+
+    const isMatch = await bcrypt.compare(password, users.hashPassword);
+    if (isMatch) res.status(201).json({ message: "Login Success" });
+    else res.status(401).json({ message: "Incorrect password" });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.listen(8080, () => {
+  console.log("Server Running");
+});
